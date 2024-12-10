2866100d240466e3fc5600dfaf0c2919cb334516
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2024-12-10 08:31:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/c/2277408829/3")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002286266637'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002384978671')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7348205141').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002470936519').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1798604396').split()]
