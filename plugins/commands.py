3bcff40e0dbf693c08a3df3e29346d65b025aca9
BotMaster135 <amirbhaiyt1@gmail.com>
2025-03-15 18:08:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -460,7 +460,7 @@ async def start(client, message):
                         InlineKeyboardButton("How To Open Link & Verify", url=VERIFY_TUTORIAL)
                     ]]
                     await message.reply_text(
-                        text="<b>You are not verified !\nKindly verify to continue\nএক বার verify করলে upto 24 ঘণ্টা পৰ্যন্ত verify করতে লাগবে না আনলিমিটেড মুভি পাবেন ❤️‍🔥 Remove Ads Get Direct File👉 /Myplan !</b>",
+                        text="<b>You are not verified !\nKindly verify to continue\nএক বার verify করলে upto 6 ঘণ্টা পৰ্যন্ত verify করতে লাগবে না আনলিমিটেড মুভি পাবেন ❤️‍🔥 Remove Ads Get Direct File👉 /Myplan !</b>",
                         protect_content=True,
                         reply_markup=InlineKeyboardMarkup(btn)
                     )
@@ -533,7 +533,7 @@ async def start(client, message):
                         InlineKeyboardButton("How To Open Link & Verify", url=VERIFY_TUTORIAL)
                     ]]
                     await message.reply_text(
-                        text="<b>You are not verified !\nKindly verify to continue\nএক বার verify করলে upto 24 ঘণ্টা পৰ্যন্ত verify করতে লাগবে না আনলিমিটেড মুভি পাবেন ❤️‍🔥 Remove Ads Get Direct File👉 /myplan !</b>",
+                        text="<b>You are not verified !\nKindly verify to continue\nএক বার verify করলে upto 6 ঘণ্টা পৰ্যন্ত verify করতে লাগবে না আনলিমিটেড মুভি পাবেন ❤️‍🔥 Remove Ads Get Direct File👉 /myplan !</b>",
                         protect_content=True,
                         reply_markup=InlineKeyboardMarkup(btn)
                     )
@@ -605,7 +605,7 @@ async def start(client, message):
                 InlineKeyboardButton("How To Open Link & Verify", url=VERIFY_TUTORIAL)
             ]]
             await message.reply_text(
-                text="<b>You are not verified !\nKindly verify to continue\nএক বার verify করলে upto 24 ঘণ্টা পৰ্যন্ত verify করতে লাগবে না আনলিমিটেড মুভি পাবেন ❤️‍🔥 Remove Ads Get Direct File👉 /Myplan !</b>",
+                text="<b>You are not verified !\nKindly verify to continue\nএক বার verify করলে upto 6 ঘণ্টা পৰ্যন্ত verify করতে লাগবে না আনলিমিটেড মুভি পাবেন ❤️‍🔥 Remove Ads Get Direct File👉 /Myplan !</b>",
                 protect_content=True,
                 reply_markup=InlineKeyboardMarkup(btn)
             )
