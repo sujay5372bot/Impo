cdfab629b7be63a972d1b22ba13894dc745e9569
BotMaster135 <amirbhaiyt1@gmail.com>
2025-03-20 22:40:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -145,7 +145,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://aggressive-evanne-jihad1350-989ba0ae.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
