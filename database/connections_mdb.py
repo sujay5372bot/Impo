a1c5e929127cb35411e97a195ce949afe1d495e1
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-03-22 07:47:21 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -5,7 +5,7 @@
 import pymongo
 
 from sample_info import tempDict
-from info import DATABASE_URI, DATABASE_NAME, SECONDDB_URI
+from info import DATABASE_URI, DATABASE_NAME, SECONDDB_URI, THIRDDB_URL
 
 import logging
 logger = logging.getLogger(__name__)
@@ -18,6 +18,10 @@ mycol = mydb['CONNECTION']
 myclient2 = pymongo.MongoClient(SECONDDB_URI)
 mydb2 = myclient2[DATABASE_NAME]
 mycol2 = mydb2['CONNECTION']
+
+myclient3 = pymongo.MongoClient(THIRDDB_URL)
+mydb3 = myclient3[DATABASE_NAME]
+mycol3 = mydb3['CONNECTION']
 
 
 async def add_connection(group_id, user_id):
@@ -40,13 +44,16 @@ async def add_connection(group_id, user_id):
         'active_group' : group_id,
     }
 
-    if mycol.count_documents( {"_id": user_id} ) == 0 and mycol2.count_documents( {"_id": user_id} ) == 0:
+    if mycol.count_documents( {"_id": user_id} ) == 0 and mycol2.count_documents( {"_id": user_id} ) == 0 and mycol3.count_documents( {"_id": user_id} ) == 0:
         try:
             if tempDict['indexDB'] == DATABASE_URI:
                 mycol.insert_one(data)
                 return True
             else:
                 mycol2.insert_one(data)
+                return True
+            else:
+                mycol3.insert_one(data)
                 return True
         except:
             logger.exception('Some error occurred!', exc_info=True)
@@ -55,6 +62,14 @@ async def add_connection(group_id, user_id):
         try:
             if mycol.count_documents( {"_id": user_id} ) == 0:
                 mycol2.update_one(
+                    {'_id': user_id},
+                    {
+                        "$push": {"group_details": group_details},
+                        "$set": {"active_group" : group_id}
+                    }
+                )
+                return True
+                mycol3.update_one(
                     {'_id': user_id},
                     {
                         "$push": {"group_details": group_details},
@@ -85,7 +100,11 @@ async def active_connection(user_id):
         { "_id": user_id },
         { "_id": 0, "group_details": 0 }
     )
-    if not query and not query2:
+    query3 = mycol3.find_one(
+        { "_id": user_id },
+        { "_id": 0, "group_details": 0 }
+    )
+    if not query and not query2 and not query3:
         return None
 
     elif query:
@@ -93,6 +112,9 @@ async def active_connection(user_id):
         return int(group_id) if group_id != None else None
     else:
         group_id = query2['active_group']
+        return int(group_id) if group_id != None else None
+    else:
+        group_id = query3['active_group']
         return int(group_id) if group_id != None else None
 
 
@@ -105,10 +127,16 @@ async def all_connections(user_id):
         { "_id": user_id },
         { "_id": 0, "active_group": 0 }
     )
+    query3 = mycol3.find_one(
+        { "_id": user_id },
+        { "_id": 0, "active_group": 0 }
+    )
     if query is not None:
         return [x["group_id"] for x in query["group_details"]]
     elif query2 is not None:
         return [x["group_id"] for x in query2["group_details"]]
+    elif query3 is not None:
+        return [x["group_id"] for x in query3["group_details"]]
     else:
         return None
 
@@ -120,6 +148,11 @@ async def if_active(user_id, group_id):
     )
     if query is None:
         query = mycol2.find_one(
+            { "_id": user_id },
+            { "_id": 0, "group_details": 0 }
+        )
+     else query2 is None:
+        query2 = mycol3.find_one(
             { "_id": user_id },
             { "_id": 0, "group_details": 0 }
         )
@@ -136,6 +169,11 @@ async def make_active(user_id, group_id):
             {'_id': user_id},
             {"$set": {"active_group" : group_id}}
         )
+     else update.modified_count == 0:
+        update = mycol3.update_one(
+            {'_id': user_id},
+            {"$set": {"active_group" : group_id}}
+        )
     return update.modified_count != 0
 
 
@@ -146,6 +184,11 @@ async def make_inactive(user_id):
     )
     if update.modified_count == 0:
         update = mycol2.update_one(
+            {'_id': user_id},
+            {"$set": {"active_group" : None}}
+        )
+    else update.modified_count == 0:
+        update = mycol3.update_one(
             {'_id': user_id},
             {"$set": {"active_group" : None}}
         )
@@ -164,10 +207,20 @@ async def delete_connection(user_id, group_id):
                 {"_id": user_id},
                 {"$pull" : { "group_details" : {"group_id":group_id} } }
             )
+        else update.modified_count == 0:
+            update = mycol3.update_one(
+                {"_id": user_id},
+                {"$pull" : { "group_details" : {"group_id":group_id} } }
+            )
             if update.modified_count == 0:
                 return False
-            else:
+            elif:
                 query = mycol2.find_one(
+                    { "_id": user_id },
+                    { "_id": 0 }
+                )
+            else:
+                query = mycol3.find_one(
                     { "_id": user_id },
                     { "_id": 0 }
                 )
@@ -179,8 +232,17 @@ async def delete_connection(user_id, group_id):
                             {'_id': user_id},
                             {"$set": {"active_group" : prvs_group_id}}
                         )
-                else:
+                        mycol3.update_one(
+                            {'_id': user_id},
+                            {"$set": {"active_group" : prvs_group_id}}
+                        )
+                elif:
                     mycol2.update_one(
+                        {'_id': user_id},
+                        {"$set": {"active_group" : None}}
+                    )
+                else:
+                    mycol3.update_one(
                         {'_id': user_id},
                         {"$set": {"active_group" : None}}
                     )
