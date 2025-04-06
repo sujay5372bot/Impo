6ebd26ff50e528a3cf65897cc4903b1e3f7c5d9b
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-04-06 09:06:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -135,9 +135,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = "".format(FQDN)
 else:
-    URL = "http://{}/".format(FQDN)
+    URL = "".format(FQDN)
 
 
 REACTIONS = ["🤝", "😇", "🤗", "😍", "👍", "🎅", "😐", "🥰", "🤩", "😱", "🤣", "😘", "👏", "😛", "😈", "🎉", "⚡️", "🫡", "🤓", "😎", "🏆", "🔥", "🤭", "🌚", "🆒", "👻", "😁"]
