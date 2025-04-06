1d85da1b551a5c5b8b5fa2557bca43b8749135cb
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-04-06 10:49:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'SilentXBotz_files')
 
 # If MULTIPLE_DB Is True Then Fill SECONDDB_URI Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), True) # Type True For Turn On MULTIPLE DB FUNTION 
-SECONDDB_URI = environ.get('SECONDDB_URI', "")
+DATABASE_URI2 = environ.get('SECONDDB_URI', "")
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/MovieSearchAutoGroup')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/NetflixHindiMOVIEHD1')
@@ -163,7 +163,7 @@ Bot_cmds = {
 #Don't Change Anything Here
 if MULTIPLE_DB == False:
     DATABASE_URI = DATABASE_URI
-    SECONDDB_URI = DATABASE_URI
+    DATABASE_URI2 = DATABASE_URI
 else:
     DATABASE_URI = DATABASE_URI
-    SECONDDB_URI = SECONDDB_URI
+    DATABASE_URI2 = SECONDDB_URI
