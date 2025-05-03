4524416b3a7fed16c35df2eed44e392be2090e34
sujay5372bot <sujay53721@gmail.com>
2025-05-03 07:09:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2995,6 +2995,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
         if NO_RESULTS_MSG:
             if any(sub in message.text.lower() for sub in ["http://", "https://", "t.me/", "telegram.me/"]):
                 await message.delete()
+                await message.reply("🚫Link Not Allow Here 🚫")  # User ko warning reply bhejein
                 return  # Skip processing
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
         k = await reply_msg.edit_text(text=script.I_CUDNT.format(mv_rqst), reply_markup=InlineKeyboardMarkup(button))
@@ -3010,6 +3011,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
         if NO_RESULTS_MSG:
             if any(sub in message.text.lower() for sub in ["http://", "https://", "t.me/", "telegram.me/"]):
                 await message.delete()
+                await message.reply("🚫Link Not Allow Here 🚫")  # User ko warning reply bhejein
                 return  # Skip processing
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
         k = await reply_msg.edit_text(text=script.I_CUDNT.format(mv_rqst), reply_markup=InlineKeyboardMarkup(button))
@@ -3039,6 +3041,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
         if NO_RESULTS_MSG:
             if any(sub in message.text.lower() for sub in ["http://", "https://", "t.me/", "telegram.me/"]):
                 await message.delete()
+                await message.reply("🚫Link Not Allow Here 🚫")  # User ko warning reply bhejein
                 return  # Skip processing
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
         k = await reply_msg.edit_text(text=script.I_CUDNT.format(mv_rqst), reply_markup=InlineKeyboardMarkup(button))
